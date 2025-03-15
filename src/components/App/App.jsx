8a4797cdea57810c811d8905c3fef6bb320eb6dc
Udav-94 <ups-fti@yandex.ru>
2025-03-15 12:07:17 +0300

    init

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+/* import { useState, useRef, useEffect } from 'react'; */
+/* import uniqid from 'uniqid'; */
+import './App.css'
+/* let elem = []; */
+
+
+function App() {
+ 
+  return (
+    <></>
+  )
+}
+
+export default App
